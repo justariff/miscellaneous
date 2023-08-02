2e43ecf419660fcdbec08a1519fce2624f5c93c6
Arif Ansari <140291737+justariff@users.noreply.github.com>
2023-08-02 12:37:07 +0530

    Update SI.py

--- basic_python_codes/SI.py
+++ basic_python_codes/SI.py
@@ -2,7 +2,7 @@ p = float(input("enter principle: "))
 r = float(input("enter rate of the interest: "))
 t = float(input("enter time period in years: "))
 
-si =  (p*r*t)/100
+si =  ( p*r*t )/100
 print("The Simple Interest of given inputs is: ",si)
 amount = p + si
 print("the amount is: ",amount)
