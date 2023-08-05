32efefcaca3898451a61c81e29b37717d14954d4
Arif Ansari <140291737+justariff@users.noreply.github.com>
2023-08-05 17:26:32 +0530

    Update compound_interest.py

--- basic_python_codes/compound_interest.py
+++ basic_python_codes/compound_interest.py
@@ -2,7 +2,7 @@ p = int(input("Enter the Principle: "))
 r = int(input("Enter the rate of interest: "))
 t = int(input("Enter the time period elapsed: "))
 
-a = p*(1 + r/100)**t
+a = p*(1 + r / 100)**t
 print("Your amount is: ",a)
 
 ci = a - p
