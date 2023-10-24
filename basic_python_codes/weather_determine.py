3b234166deea5158adfa69d75db0b0ad530d8230
Arif Ansari <140291737+justariff@users.noreply.github.com>
2023-10-24 23:25:25 +0530

    Update weather_determine.py

--- basic_python_codes/weather_determine.py
+++ basic_python_codes/weather_determine.py
@@ -1,7 +1,7 @@
 temp = float(input("enter the Temperature: "))
 hum = float(input("enter the humidity: "))
 
-if temp>=30 and hum>=90:
+if temp >= 30 and hum>=90:
     print("Weather is Hot and Humid")
 elif temp>=30 and hum<90:
     print("Weather is Hot")
